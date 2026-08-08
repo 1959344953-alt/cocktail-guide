4394fef43b71acaa9eae5f636f4b4c0f2e058650
1959344953-alt <1959344953@qq.com>
2026-08-08 23:10:11 +0800

    百度站点验证：验证文件 + head meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="zh-CN">
       <head>
         <meta name="msvalidate.01" content="E2F6731EDB918688BD5C7F888A3A4C41" />
+        <meta name="baidu-site-verification" content="codeva-1dm973mS8v" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
